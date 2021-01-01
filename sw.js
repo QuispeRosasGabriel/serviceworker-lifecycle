4b5d2193bb5aa8a580990e4570c0bfb2e912b655
GabrielQuispe <gabrielomarquisperosas@gmail.com>
2021-01-01 11:46:04 -0500

    creating install step

--- sw.js
+++ sw.js
@@ -1,3 +1,10 @@
 
 // Ciclo de vida del SW
 
+// En la instalación
+self.addEventListener('install',(event) => {
+    // Descargamos assets
+    // Creamos caché
+    console.log('Instalando service worker');
+
+});
