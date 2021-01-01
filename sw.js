8681e1e920d909a15c782b226653a4c9ba3c7c80
GabrielQuispe <gabrielomarquisperosas@gmail.com>
2021-01-01 12:04:38 -0500

    wait until

--- sw.js
+++ sw.js
@@ -2,16 +2,23 @@
 // Ciclo de vida del SW
 
 // En la instalación
-self.addEventListener('install',(event) => {
+self.addEventListener('install', (event) => {
     // Descargamos assets
     // Creamos caché
     console.log('Instalando service worker');
+    const installation = new Promise((res, rej) => {
+        setTimeout(() => {
+            console.log('instalaciones terminadas');
+            self.skipWaiting();
+            res();
+        }, 1000);
+    });
 
+    event.waitUntil(installation);
 });
 
 // En la activación
 self.addEventListener('activate', (event) => {
     // Borrar caché viejo
     console.log('Service worker activado y listo');
-
 })
